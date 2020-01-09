d4f4246e7319573f0aa5fdd43ca21b5ba7e073bf
DanielErick-010 <57202243+DanielErick-010@users.noreply.github.com>
2020-01-09 18:11:07 +0700

    Update Database

---
+++ src/config/Database.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package config;
+
+/**
+ *
+ * @author Daniel
+ */
+public interface Database {
+    public String dbHost = "localhost";
+    public String dbName = "supermarket";
+    public String dbUser = "root";
+    public String dbPass = "";
+
+}
---
+++ src/model/MySQLConnection.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author Daniel
+ */
+public class MySQLConnection implements config.Database {
+    
+    public Connection conn;
+
+    public MySQLConnection (){
+        try {
+            String dbURL = "jdbc:mysql://"+ this.dbHost +":3306/" + this.dbName;
+            this.conn = DriverManager.getConnection(dbURL, this.dbUser, this.dbPass);
+            if (this.conn != null) {
+//                System.out.println("Connected");
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+    public void close(){
+        try {
+            this.conn.close();
+        } catch (SQLException ex) {
+            System.out.println("Penutupan koneksi gagal");
+        }
+    }
+    
+    public static void main(String[] args) {
+        MySQLConnection test = new MySQLConnection();
+        
+    }
+
+}
