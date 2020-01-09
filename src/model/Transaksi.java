f84f4b53c54abbe7746a4bfb9b58af652d0a6481
Ahnaffaiz <faiz.putra553@gmail.com>
2020-01-10 06:01:17 +0700

    update class transaksi

--- src/model/Transaksi.java
+++ src/model/Transaksi.java
@@ -7,9 +7,7 @@ package model;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 /**
  *
@@ -33,6 +31,7 @@ public class Transaksi {
     }
 
     public static void insert(Integer id_keranjang, String tanggal, Integer total, Integer pembayaran, Integer kembalian) {
+        
         // lakukan koneksi ke mysql
         MySQLConnection m = new MySQLConnection();
         Connection koneksi = m.conn;
@@ -63,89 +62,7 @@ public class Transaksi {
             System.out.println("Insert data transaksi gagal");
             m.close();
         }
-    }
 
-    public static void select(Integer id_keranjang) {
-
-        // query sql untuk select data transaksi
-        String sql = "SELECT t.id_transaksi, t.tanggal,GROUP_CONCAT(k.qty_barang,' ',b.nama_barang SEPARATOR '/n ') barang, t.total from db_transaksi t, db_keranjang k, db_barang b where t.id_keranjang=k.id_keranjang and k.id_barang=b.id_barang and t.id_keranjang = " + id_keranjang + " GROUP BY id_transaksi";
-
-        // lakukan koneksi ke mysql
-        MySQLConnection m = new MySQLConnection();
-        Connection koneksi = m.conn;
-
-        try {
-            Statement statement = koneksi.createStatement();
-            // jalankan query
-            ResultSet result = statement.executeQuery(sql);
-
-            // membuat header table untuk output
-            System.out.println("==============================================================================");
-            String header = "%3s %15s %20s %20s";
-            System.out.println(String.format(header, "ID", "TANGGAL", "BARANG", "TOTAL"));
-            System.out.println("------------------------------------------------------------------------------");
-
-            // looping untuk baca data per record
-            while (result.next()) {
-                // baca data transaksi per record
-                String idTransaksi = result.getString("id_transaksi");
-                String namaPembeli = result.getString("tanggal");
-                String namaBarang = result.getString("barang");
-                String totalHarga = result.getString("total");
-
-                // tampilkan data transaksi per record
-                String output = "%2s %10s %35s %20s";
-                System.out.println(String.format(output, idTransaksi, namaPembeli, namaBarang, totalHarga));
-            }
-
-            System.out.println("==============================================================================");
-            m.close();
-        } catch (SQLException ex) {
-            System.out.println("Tampil data Barang gagal");
-            m.close();
-        }
-
-    }
-
-    public static void select() {
-
-        // query sql untuk select all data transaksi
-        String sql = "SELECT t.id_transaksi, t.tanggal,GROUP_CONCAT(k.qty_barang,' ',b.nama_barang SEPARATOR ', ') barang, t.total from db_transaksi t, db_keranjang k, db_barang b where t.id_keranjang=k.id_keranjang and k.id_barang=b.id_barang GROUP BY id_transaksi";
-
-        // lakukan koneksi ke mysql
-        MySQLConnection m = new MySQLConnection();
-        Connection koneksi = m.conn;
-
-        try {
-            Statement statement = koneksi.createStatement();
-            // jalankan query
-            ResultSet result = statement.executeQuery(sql);
-
-            // membuat header table untuk output
-            System.out.println("==============================================================================");
-            String header = "%3s %15s %20s %20s";
-            System.out.println(String.format(header, "ID", "TANGGAL", "BARANG", "TOTAL"));
-            System.out.println("------------------------------------------------------------------------------");
-
-            // looping untuk baca data per record
-            while (result.next()) {
-                // baca data buku per record
-                String idTransaksi = result.getString("id_transaksi");
-                String namaPembeli = result.getString("nama_pembeli");
-                String namaBarang = result.getString("barang");
-                String totalHarga = result.getString("total");
-
-                // tampilkan data buku per record
-                String output = "%2s %10s %35s %20s";
-                System.out.println(String.format(output, idTransaksi, namaPembeli, namaBarang, totalHarga));
-            }
-
-            System.out.println("==============================================================================");
-            m.close();
-        } catch (SQLException ex) {
-            System.out.println("Tampil data Barang gagal");
-            m.close();
-        }
 
     }
 }
