2807e34f424a22aa1854e842f577f7b959e5f566
Ahnaffaiz <faiz.putra553@gmail.com>
2020-01-10 08:12:43 +0700

    add nota

--- src/process/Controller.java
+++ src/process/Controller.java
@@ -56,10 +56,14 @@ public class Controller {
     }
 
     public void beliBarang() {
+        CommandLineTable st = new CommandLineTable();
+        st.setShowVerticalLines(true);
         String beliLagi = "y";
         Integer totalTransaksi = 0;
+        Integer noNota = 0;
         Integer idKeranjang = new Random().nextInt();
         while (beliLagi.toLowerCase().equals("y")) {
+            noNota += 1;
             System.out.println("Masukkan ID Barang");
             Integer idBarang = sc.nextInt();
             Map<String, String> barang = Barang.select(idBarang);
@@ -72,6 +76,10 @@ public class Controller {
             Integer total = hrgBarang * qtyBarang;
             Keranjang.insert(idKeranjang, idBarang, qtyBarang, total);
             totalTransaksi += total;
+            //header nota
+            st.setHeaders("No","Barang", "Qty", "Harga");
+            //isi nota
+            st.addRow(noNota.toString(), nmBarang, qtyBarang.toString(), total.toString());
             System.out.println("Beli lagi ?");
             beliLagi = sc.next();
             Integer sisa = stkBarang - qtyBarang;
@@ -79,20 +87,22 @@ public class Controller {
         }
 
         Keranjang.select(Integer.valueOf(idKeranjang));
-        System.out.println("Uang yang di bayarkan : ");
+        System.out.print("Uang yang di bayarkan : ");
         Integer pembayaran = sc.nextInt();
         Integer kembalian = pembayaran - totalTransaksi;
         System.out.println("Kembalian : " + kembalian);
         Transaksi.insert(idKeranjang, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(now), totalTransaksi, pembayaran, kembalian);
-        
+        System.out.println("                  Nota Pembayaran                    ");
+        System.out.println("=====================================================");
+        System.out.print("ID Transaksi: ");
+        Transaksi.selectId(idKeranjang);
+        System.out.print("Tanggal Transaksi: ");
+        Transaksi.selectTanggal(idKeranjang);
+        System.out.println(" ");
+        st.print();
+        System.out.println("=====================================================");
         if (!beliLagi.toLowerCase().equals("y")) {
             new MainClass().jalankan();
         }
     }
-
-//    public static void main(String[] args) {
-////        this.beliBarang();
-//        Controller ctr = new Controller();
-//        ctr.beliBarang();
-//    }
 }
